6548859c73544efe6a107ed35a8ac671eeec25c1
John Ryan <jpr269@cornell.edu>
2018-11-15 11:17:54 -0500

    skelfac.h add

--- ie-solver/skelfac.h
+++ ie-solver/skelfac.h
@@ -1,5 +1,5 @@
-#ifndef _SKELFAC_H_
-#define _SKELFAC_H_
+#ifndef _IE_SOLVER_TOOLS_H_
+#define _IE_SOLVER_TOOLS_H_
 #include <mutex>
 #include <omp.h>
 #include "common.h"
@@ -9,21 +9,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MIN_DOFS_TO_COMPRESS 16
+
 namespace ie_solver{
 
-class Skelfac {
+class IeSolverTools {
 
 	public:
-		Skelfac(){}
-		Skelfac(double id_tol, const std::vector<double> points, 
+
+		Clock set_get;
+		double id_tol;
+		//necessary for proxy creation
+		std::vector<double> points, normals, weights; 
+		std::mutex lock;
+		bool is_stokes;
+
+		IeSolverTools(){}
+		IeSolverTools(double id_tol, const std::vector<double> points, 
 			const std::vector<double> normals, 
 			const std::vector<double> weights, bool is_stokes);
-		~Skelfac(){}
+		~IeSolverTools(){}
 
 		// The actual work functions
 		void GetXMatrices(ie_Mat& K, const ie_Mat& Z, ie_Mat&,
-		const std::vector<unsigned int>& r, const std::vector<unsigned int>& s,
-		const std::vector<unsigned int>& n);
+			const std::vector<unsigned int>& r, 
+			const std::vector<unsigned int>& s,
+			const std::vector<unsigned int>& n);
+		
 		void SchurUpdate(const ie_Mat& K, const ie_Mat& Z, ie_Mat& L, ie_Mat& U, 
 			QuadTreeNode* node);
 		int InterpolativeDecomposition(const ie_Mat& K, ie_Mat& Z, 
@@ -50,27 +62,21 @@ class Skelfac {
 			ie_Mat& b);
 		void Solve(const ie_Mat& K, QuadTree& tree, ie_Mat& x, const ie_Mat& b);
 
-	    // void Conjugate_Gradient(ie_Mat& F, QuadTree& tree, ie_Mat& phi, 
-	    // 	ie_Mat& f);
+
 		void remove_inactive_dofs(QuadTreeNode*);
 		// TODO when generalizing, let someone pass these functions in initing
-		// skelfac
+		// IeSolverTools
 		void make_proxy_mat(ie_Mat& pxy, double cntr_x, double cntr_y, double r, 
 			const std::vector<unsigned int>& range);
 		void make_stokes_proxy_mat(ie_Mat& pxy, double cntr_x, double cntr_y, 
 			double r, const std::vector<unsigned int>& range);
 
-		void set_rs_ranges(Box& box, const std::vector<unsigned int>& prm, 
+		void set_rs_ranges(InteractionLists& interaction_lists, const std::vector<unsigned int>& prm, 
 			unsigned int sk, unsigned int rd);
-		void set_skelnear_range(Box& box);
+		void set_skelnear_range(InteractionLists& interaction_lists);
+		// void Conjugate_Gradient(ie_Mat& F, QuadTree& tree, ie_Mat& phi, 
+	    // 	ie_Mat& f);
 
-		Clock set_get;
-		double id_tol = 0.001;
-		std::vector<double> points, normals, weights; 
-		//necessary for proxy creation
-
-        std::mutex lock;
-		bool is_stokes_;
 };
 
 } // namespace ie_solver
