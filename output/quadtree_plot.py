86e73c0dda0f2973baa5959884982c8c2f2f7310
John Ryan <jpr269@cornell.edu>
2019-09-12 13:55:05 -0400

    new plots

--- output/quadtree_plot.py
+++ output/quadtree_plot.py
@@ -9,7 +9,7 @@ import matplotlib.pyplot as plt
 
 WINDOW_SIZE = 140*5
 IMAGE_SIZE = 100*5
-CMAP = copy(matplotlib.cm.hot)
+CMAP = copy(matplotlib.cm.magma)
 CMAP.set_bad('lightgray', 1.)
 MASKED_VALUE = 11111.1
 fig = plt.figure(figsize=(14,14))
@@ -28,7 +28,7 @@ CENTER = WINDOW_SIZE/2.0
 #							READING THE FILES
 #
 ###########################################################
-solution_lines = open("output/data/ie_solver_solution.txt","r").readlines()
+tree_lines = open("output/data/ie_solver_tree.txt","r").readlines()
 boundary_lines = open("output/data/ie_solver_boundary.txt","r").readlines()
 
 boundary_points = []
@@ -36,19 +36,18 @@ for line in boundary_lines:
 	linesplit = line.split(',')
 	boundary_points.append([float(linesplit[0]), float(linesplit[1])])
 solution_points = []
-is_stokes = False
-if len(solution_lines[0].split(",")) == 4:
-	is_stokes = True
-for line in solution_lines:
-	linesplit = line.split(',')
-	if(is_stokes):
-		solution_points.append([float(linesplit[0]), float(linesplit[1]),
-			float(linesplit[2]), float(linesplit[3])])
+
+quadtree_data = []
+current_level_data = []
+for line in tree_lines[1:]:
+	if "level " in line:
+		quadtree_data.append(current_level_data)
+		current_level_data = []
 	else:
-		solution_points.append([float(linesplit[0]), float(linesplit[1]),
-			float(linesplit[2])])
-
-
+		linesplit = line.split(',')
+		current_level_data.append([float(linesplit[0]), float(linesplit[1]),
+			float(linesplit[2]), float(linesplit[3])])
+num_levels = len(quadtree_data)
 ###########################################################
 #
 #							SCALING THE PLOT
@@ -112,48 +111,41 @@ def draw_boundary(img, points, val):
 			  img[int(x_coord)][int(y_coord)] = val
 
 
-def draw_solution(img, points):
-	for point in points:
-		pixel = scaled_point(point[:2])
-		if(np.isnan(point[2]) or point[2] == 0):
-			img[pixel[0]][pixel[1]] = MASKED_VALUE
-		else:
-			for i in range(-8,9):
-				for j in range(-8,9):
-				  
-				  x_zoom = (pixel[0] - CENTER)*ZOOM + CENTER + SHIFT
-				  y_zoom = (pixel[1] - CENTER)*ZOOM + CENTER
-				  if x_zoom+i < 0 or x_zoom+i > WINDOW_SIZE-1 or y_zoom+j < 0 or y_zoom+j > WINDOW_SIZE-1:
-				    continue
-				  x_coord = max(0,min(WINDOW_SIZE-1, x_zoom+i))
-				  y_coord = max(0,min(WINDOW_SIZE-1, y_zoom+j))
-				  img[int(x_coord)][int(y_coord)] = point[2]
-				  
+def draw_box(img, datum):
+	side_length = datum[2]
+	bottom_left = datum[:2]
+
+	compression_ratio = datum[3]
+	# time_data = datum[4]
+
+	top_right = [bottom_left[0] + side_length, bottom_left[1] + side_length]
+	bottom_left = scaled_point(bottom_left)
+	top_right = scaled_point(top_right)
+	for x in range(bottom_left[0], top_right[0]+1):
+		for y in [bottom_left[1], top_right[1]]:
+			for r in range(-1, 2):
+				for c in range(-1,2):
+				  x_coord = max(0, min(x+r, len(img)-1))
+				  y_coord = max(0, min(y+c, len(img[0])-1))
+				  img[x_coord][y_coord] = 1.0
+
+	for x in [bottom_left[0], top_right[0]]:
+		for y in range(bottom_left[1], top_right[1]+1):
+			for r in range(-1, 2):
+				for c in range(-1,2):
+				  x_coord = max(0, min(x+r, len(img)-1))
+				  y_coord = max(0, min(y+c, len(img[0])-1))
+				  img[x_coord][y_coord] = 1.0
+
+	for x in range(bottom_left[0]+1, top_right[0]):
+		for y in range(bottom_left[1]+1, top_right[1]):
+			img[x][y] = compression_ratio
 
 
-def get_quiver_data(points):
-	# returns an array containing the four vecs necessary for a quiver plot
-	# This can be replaced with vecs with colon index probably TODO
-	X = []
-	Y = []
-	U = []
-	V = []
-	colors = []
-	for point in points:
-		pixel = scaled_point(point[:2])
-		x_zoom = (pixel[0]  - CENTER)*ZOOM + CENTER+SHIFT
-		y_zoom = (pixel[1]  - CENTER)*ZOOM + CENTER
-		if x_zoom < 0 or x_zoom > WINDOW_SIZE-1 or y_zoom < 0 or y_zoom > WINDOW_SIZE-1:
-		  continue
-		x_coord = max(0,min(WINDOW_SIZE-1, x_zoom))
-		y_coord = max(0,min(WINDOW_SIZE-1, y_zoom))
-		
-		X.append(x_coord)
-		Y.append(y_coord)
-		U.append(point[2])
-		V.append(point[3])
-		colors.append(point[2]**2 + point[3]**2)
-	return [X, Y, U, V, colors]
+def draw_quadtree(img, quadtree_data):
+	# get corners
+	for datum in quadtree_data:
+		draw_box(img, datum)
 
 ###########################################################
 #
@@ -162,29 +154,39 @@ def get_quiver_data(points):
 #
 #
 ###########################################################
+# TREE DECOMPOSITION PLOT
+current_shown_level = 0
+tree_images = []
+for i in range(num_levels):
+	tree_img = np.array([[MASKED_VALUE for x in range(WINDOW_SIZE)] for y in range(WINDOW_SIZE)])
+	draw_quadtree(tree_img, quadtree_data[i])
+	draw_boundary(tree_img, boundary_points, val=1.0)
+	tree_img = np.ma.masked_where(tree_img == MASKED_VALUE, tree_img)
+	tree_images.append(tree_img)
 
-# SOLUTION PLOT
-solution_img = np.array([[MASKED_VALUE for x in range(WINDOW_SIZE)] for y in range(WINDOW_SIZE)])
-stokes_data = []
-if(is_stokes):
-	draw_boundary(solution_img, boundary_points, val=1.0)
-	stokes_data = get_quiver_data(solution_points)
-else:
-	# draw_boundary(solution_img, boundary_points, val=1.0)
-	draw_solution(solution_img, solution_points)
-solution_img = np.ma.masked_where(solution_img == MASKED_VALUE, solution_img)
-plt.title("Solution")
-plt.imshow(solution_img.T, cmap=CMAP, origin = "lower")
-if(is_stokes):
-	plt.quiver(stokes_data[0], stokes_data[1], stokes_data[2], stokes_data[3],
-		stokes_data[4], cmap = "Purples", #cmap='autumn',
-		norm=matplotlib.colors.Normalize(vmin=0,vmax=1.), scale=20./ZOOM)
+plt.title("Quadtree")
+im = plt.imshow(tree_images[current_shown_level].T, cmap=CMAP, vmin=0., vmax=1., origin = "lower")
+plt.colorbar(im)
 
 ############################################################
 #
 #						INTERACTIVITY
 #
 ############################################################
+
+def on_key(event):
+	global current_shown_level, im
+
+	if(event.key == "down" and current_shown_level < num_levels-1):
+		current_shown_level += 1
+		im.set_data(tree_images[current_shown_level].T)
+		plt.draw()
+	elif(event.key == "up" and current_shown_level > 0):
+		current_shown_level -= 1
+		im.set_data(tree_images[current_shown_level].T)
+		plt.draw()
+
+
 # def onclick(event):
 # 	print("******************************\nCLICKED!")
 # 	x = event.xdata
@@ -197,6 +199,6 @@ if(is_stokes):
 #   #       ('double' if event.dblclick else 'single', event.button,
 #   #        event.x, event.y, event.xdata, event.ydata))
 
-# cid = fig.canvas.mpl_connect('button_press_event', onclick)
-plt.savefig("domain_plot.png")
+cid = fig.canvas.mpl_connect('key_press_event', on_key)
+plt.savefig("quadtree_plot.png")
 plt.show()
